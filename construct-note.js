c07228f7739c58b7142b8ec7ffa037c5851e754a
Brian Shapiro <briandavidshapiro@gmail.com>
2021-02-24 18:06:45 -0500

    completed everything before further study

--- construct-note.js
+++ construct-note.js
@@ -1,3 +1,21 @@
 // add whatever parameters you deem necessary
-function constructNote() {
+function constructNote(msg, chars) {
+	let msgCounter = makeCounter(msg);
+	let charsCounter = makeCounter(chars);
+	let msgChars = Object.keys(msgCounter);
+	for (let char of msgChars) {
+		if (!charsCounter[char] || charsCounter[char] < msgCounter[char])
+			return false;
+	}
+	return true;
 }
+
+function makeCounter(str) {
+	let counter = {};
+	for (let char of str) {
+		counter[char] = counter[char] + 1 || 1;
+	}
+	return counter;
+}
+
+module.exports = constructNote;
--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,28 @@
 // add whatever parameters you deem necessary
-function isSubsequence() {
+function isSubsequence(str1, str2) {
+	if (str1 === str2) return true;
+	if (str1.length >= str2.length) return false;
+
+	let str2Tracker = makeOrderTracker(str2);
+	for (let i = 0; i < str1.length - 1; i++) {
+		let char = str1[i];
+		let nextChar = str1[i + 1];
+		if (str2Tracker[char] === undefined || str2Tracker[nextChar] === undefined)
+			return false;
+		if (nextChar !== char) {
+			if (str2Tracker[char] > str2Tracker[nextChar]) return false;
+		}
+	}
+	return true;
 }
+
+function makeOrderTracker(str) {
+	let tracker = {};
+	for (let i = 0; i <= str.length - 1; i++) {
+		let char = str[i];
+		if (tracker[char] === undefined) tracker[char] = i;
+	}
+	return tracker;
+}
+
+module.exports = isSubsequence;
